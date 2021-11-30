bfbf923bde72a6f786b47601692625b41b24f718
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-11-30 14:09:00 +0200

    Basic server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-/* write your server code here */
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.get('/', (req, res) => {
+	res.send('test server');
+});
+
+app.listen(port, function () {
+	console.log('server running💨💨');
+});
